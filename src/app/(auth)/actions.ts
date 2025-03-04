2c122d9e19487bf30b1b7299aa1fbb16bd1fdc57
Naeem Baghi <baghinaeem@gmail.com>
2025-03-04 22:23:42 +0330

    feat: add authorization actions

---
+++ src/app/(auth)/actions.ts
@@ -0,0 +1,54 @@
+"use server";
+
+import {
+  LoginFormActionState,
+  RegisterFormActionState,
+  registerUserSchema,
+} from "@/features/authentication/types";
+import systemSdk from "@/gql/system/sdk";
+import { signIn } from "@/lib/auth";
+
+export const register = async (
+  _: RegisterFormActionState,
+  data: FormData,
+): Promise<RegisterFormActionState> => {
+  try {
+    const { firstName, lastName, email, password } = Object.fromEntries(
+      data.entries(),
+    );
+
+    const parsed = registerUserSchema.safeParse({
+      firstName,
+      lastName,
+      email,
+      password,
+    });
+
+    if (!parsed.success) {
+      return parsed.error.flatten().fieldErrors;
+    }
+
+    await systemSdk.register({
+      data: {
+        first_name: parsed.data.firstName,
+        last_name: parsed.data.lastName,
+        email: parsed.data.email,
+        password: parsed.data.password,
+      },
+    });
+    return { success: true };
+  } catch (error) {
+    console.log({ error });
+    return { email: ["Email already exists"] };
+  }
+};
+
+export const login = async (_: LoginFormActionState, data: FormData) => {
+  try {
+    await signIn("credentials", data);
+    return { success: true };
+  } catch (error) {
+    console.log({ error });
+    return { error: "Invalid Credentials" };
+  }
+};
--- src/features/authentication/types.ts
+++ src/features/authentication/types.ts
@@ -1,5 +1,22 @@
 import { z } from "zod";
 
+export type LoginFormActionState =
+  | { success: true }
+  | {
+      email?: string[];
+      password?: string[];
+      error?: string;
+    };
+
+export type RegisterFormActionState =
+  | { success: true }
+  | {
+      email?: string[];
+      password?: string[];
+      firstName?: string[];
+      lastName?: string[];
+    };
+
 export const loginUserSchema = z.object({
   email: z.string().email(),
   password: z.string().min(1),
